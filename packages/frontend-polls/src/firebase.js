23cf80b58b830afdf409952796a18691e35d550c
Miguel Ángel Durán <miduga@gmail.com>
2018-10-26 00:17:54 +0200

    Create bunch of methods on firebase master file

--- packages/frontend-polls/src/firebase.js
+++ packages/frontend-polls/src/firebase.js
@@ -15,4 +15,58 @@ firebase.initializeApp(config)
 const db = firebase.firestore()
 db.settings({timestampsInSnapshots: true})
 
-export {firebase, db}
+const votesCollection = db.collection('votes')
+
+function onAuthStateChanged(handler) {
+  firebase.auth().onAuthStateChanged(userObject => {
+    const user = userObject ? userObject.uid : false
+    handler(user)
+  })
+}
+
+function onGetVotesByProposal({proposal}, handler) {
+  votesCollection
+    .where('proposal', '==', proposal)
+    .onSnapshot(querySnapshot => {
+      let votes = []
+      if (querySnapshot.docs.length > 0) {
+        querySnapshot.forEach(function(doc) {
+          const {vote} = doc.data()
+          votes.push(vote)
+        })
+      }
+      handler(votes)
+    })
+}
+
+function onGetVotesByUser({user}, handler) {
+  votesCollection.where('user', '==', user).onSnapshot(querySnapshot => {
+    let votes = {}
+    if (querySnapshot.docs.length > 0) {
+      querySnapshot.forEach(function(doc) {
+        const {proposal, vote} = doc.data()
+        votes[proposal] = vote
+      })
+    }
+    handler(votes)
+  })
+}
+
+function setVote({proposal, user, vote}) {
+  const votesPath = `${proposal}_${user}`
+  votesCollection.doc(votesPath).set({proposal, user, vote})
+}
+
+function signInAnonymously() {
+  return firebase.auth().signInAnonymously()
+}
+
+export {
+  firebase,
+  db,
+  onAuthStateChanged,
+  onGetVotesByProposal,
+  onGetVotesByUser,
+  setVote,
+  signInAnonymously
+}
