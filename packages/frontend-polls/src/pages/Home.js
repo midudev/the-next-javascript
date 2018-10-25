cfb3f8d6d5085e0eac838af388b8e27a0dee69be
Miguel Ángel Durán <miduga@gmail.com>
2018-10-26 00:17:36 +0200

    React.memo for Home

--- packages/frontend-polls/src/pages/Home.js
+++ packages/frontend-polls/src/pages/Home.js
@@ -1,24 +1,21 @@
 /* eslint-disable react/prop-types */
 
-import React, {Component} from 'react'
+import React from 'react'
 import AtomSpinner, {AtomSpinnerTypes} from '@s-ui/react-atom-spinner'
 
 import {Proposals} from '../components/Proposals'
 import {Header} from '../components/Header'
 
-export class Home extends Component {
-  render() {
-    const {user} = this.props
-    return (
-      <section className="js-HomePage">
-        <Header />
-        {user && <Proposals user={user} />}
-        {!user && (
-          <div className="js-HomePage-loading">
-            <AtomSpinner type={AtomSpinnerTypes.FULL} />
-          </div>
-        )}
-      </section>
-    )
-  }
-}
+export const Home = React.memo(function({user}) {
+  return (
+    <section className="js-HomePage">
+      <Header />
+      {user && <Proposals user={user} />}
+      {!user && (
+        <div className="js-HomePage-loading">
+          <AtomSpinner type={AtomSpinnerTypes.FULL} />
+        </div>
+      )}
+    </section>
+  )
+})
